c328fb320de353ed5ec3a8bed3895553fdc0f49c
saihariG <krishnansaihari@gmail.com>
2022-04-20 21:13:49 +0530

    Handled credential invalidations

--- MobiX/ViewControllers/ViewController.swift
+++ MobiX/ViewControllers/ViewController.swift
@@ -14,14 +14,18 @@ class ViewController: UIViewController,UITextFieldDelegate {
     @IBOutlet weak var emailField: UITextField!
     @IBOutlet weak var passField: UITextField!
     
+    @IBOutlet weak var alertText: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         self.emailField.delegate = self
         self.passField.delegate = self
         
-        emailField.text = "saihari@mobicip.com"
-        passField.text = "1234"
+        alertText.isHidden = true
+        
+       // emailField.text = "saihari@mobicip.com"
+       // passField.text = "1234"
     }
 
     // To hide keyboard on touch
@@ -48,25 +52,46 @@ class ViewController: UIViewController,UITextFieldDelegate {
 
     @IBAction func loginClick(_ sender: Any) {
 
-        let mail = emailField.text
-        let pass = passField.text
+        let mail = emailField.text?.trimmingCharacters(in: .whitespaces)
+        let pass = passField.text?.trimmingCharacters(in: .whitespaces)
         
         var i  = 0
+        var isRegistered = false
+        
+        if(mail == "" && pass == "") {
+            alertText.isHidden = false
+            alertText.text = "Empty Field!"
+            return
+        }
+        
+        if(mail != "" && pass == "") || (mail == "" && pass != "") {
+            alertText.text = "Empty Field"
+            return
+        }
+        
+        alertText.isHidden = true
+        
+        if(!isValidEmailAddress(emailAddressString: mail!) && mail != "" ) {
+            alertText.isHidden = false
+            alertText.text = "invalid mail format"
+            return
+        }
         
         for k in 0...allUsers.users.count-1 {
-            print(allUsers.users[k].mailID)
+           // print(allUsers.users[k].mailID)
             if mail == allUsers.users[k].mailID {
+                isRegistered = true
                 i = k
-                print("registered")
                 break
-            }
-            else {
-                return
             }
         }
         
+        if !isRegistered && mail != "" {
+          alertText.isHidden = false
+          alertText.text = "email not registered!"
+        }
             
-        if isValidEmailAddress(emailAddressString: mail!) {
+        if isValidEmailAddress(emailAddressString: mail!) && isRegistered {
             //print("valid mail \(mail!)")
             // if mail is valid , check password
             if pass == allUsers.users[i].password {
@@ -75,7 +100,8 @@ class ViewController: UIViewController,UITextFieldDelegate {
                 self.navigationController?.pushViewController(vc, animated: true)
             }
             else {
-                print("Invalid Password")
+                alertText.isHidden = false
+                alertText.text = "Invalid Password!"
             }
         }
         
